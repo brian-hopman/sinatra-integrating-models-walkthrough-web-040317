ea104b44efa77717edc2903bdf3c9195db1cceb1
brian-hopman <hopman_2@hotmail.com>
2017-04-25 17:40:51 -0400

    Done.

--- app.rb
+++ app.rb
@@ -7,8 +7,14 @@ get '/' do
   erb :index
 end
 
+# post '/' do
+#    text_from_user = params[:user_text]
+#
+#    erb :results
+#  end
+
 post '/' do
-  text_from_user = params[:user_text]
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
   erb :results
 end
 
